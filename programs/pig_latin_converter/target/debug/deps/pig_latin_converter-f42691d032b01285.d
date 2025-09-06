ca20bef377963930620db3e6437f09dd78131615
Lukas Hammer <lukas@minipli.net>
2025-09-07 00:00:21 +0200

    Add new program

---
+++ programs/pig_latin_converter/target/debug/deps/pig_latin_converter-f42691d032b01285.d
@@ -0,0 +1,5 @@
+/Users/lukashammer/my-rust-learning-journey/programs/pig_latin_converter/target/debug/deps/pig_latin_converter-f42691d032b01285.d: src/main.rs
+
+/Users/lukashammer/my-rust-learning-journey/programs/pig_latin_converter/target/debug/deps/pig_latin_converter-f42691d032b01285: src/main.rs
+
+src/main.rs:
---
+++ programs/pig_latin_converter/target/debug/pig_latin_converter.d
@@ -0,0 +1,1 @@
+/Users/lukashammer/my-rust-learning-journey/programs/pig_latin_converter/target/debug/pig_latin_converter: /Users/lukashammer/my-rust-learning-journey/programs/pig_latin_converter/src/main.rs
