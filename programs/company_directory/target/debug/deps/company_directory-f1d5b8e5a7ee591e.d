1d39b4bb621fbe1c5abcafbf2b2ca2e4cfca3fb4
Lukas Hammer <lukas@minipli.net>
2025-09-06 15:56:29 +0200

    Delete programs/company_directory/target directory

--- programs/company_directory/target/debug/deps/company_directory-f1d5b8e5a7ee591e.d
+++
@@ -1,5 +0,0 @@
-/Users/lukashammer/my-rust-learning-journey/company_directory/target/debug/deps/company_directory-f1d5b8e5a7ee591e.d: src/main.rs
-
-/Users/lukashammer/my-rust-learning-journey/company_directory/target/debug/deps/libcompany_directory-f1d5b8e5a7ee591e.rmeta: src/main.rs
-
-src/main.rs:
