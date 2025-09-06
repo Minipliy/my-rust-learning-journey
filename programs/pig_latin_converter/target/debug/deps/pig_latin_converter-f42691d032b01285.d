3ee805d1e8d3355e0f50b6022c36177d554ad79d
Lukas Hammer <lukas@minipli.net>
2025-09-07 00:16:09 +0200

    Delete programs/pig_latin_converter/target directory

--- programs/pig_latin_converter/target/debug/deps/pig_latin_converter-f42691d032b01285.d
+++
@@ -1,5 +0,0 @@
-/Users/lukashammer/my-rust-learning-journey/programs/pig_latin_converter/target/debug/deps/pig_latin_converter-f42691d032b01285.d: src/main.rs
-
-/Users/lukashammer/my-rust-learning-journey/programs/pig_latin_converter/target/debug/deps/pig_latin_converter-f42691d032b01285: src/main.rs
-
-src/main.rs:
--- programs/pig_latin_converter/target/debug/pig_latin_converter.d
+++
@@ -1,1 +0,0 @@
-/Users/lukashammer/my-rust-learning-journey/programs/pig_latin_converter/target/debug/pig_latin_converter: /Users/lukashammer/my-rust-learning-journey/programs/pig_latin_converter/src/main.rs
